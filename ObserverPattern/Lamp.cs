3de47f964aafb607cec698e5ea52e066b4442a0c
Dusan Biocanin <dusanbiocanin@hotmail.com>
2018-03-21 12:21:59 +0100

    1st commit

---
+++ ObserverPattern/Lamp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+	public class Lamp : IObserver
+	{
+
+
+		public void Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ ObserverPattern/MusicPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+	public enum GridState { On, Off }
+
+	public class PowerGrid : IObservable
+	{
+		private IList<IObserver> observers;
+		private GridState gridState { get; set; }
+
+		public PowerGrid()
+		{
+			this.observers = new List<IObserver>();
+			this.gridState = GridState.Off;
+		}
+
+		public void Add(IObserver observer)
+		{
+			this.observers.Add(observer);
+		}
+
+		public void Notify()
+		{
+			foreach (var observer in this.observers)
+			{
+				observer.Update();
+			}
+		}
+
+		public void Remove(IObserver observer)
+		{
+			this.observers.Remove(observer);
+		}
+
+		public void ChangeState()
+		{
+			if (this.gridState == GridState.Off)
+			{
+				this.gridState = GridState.On;
+			}
+			else
+			{
+				this.gridState = GridState.Off;
+			}
+
+			Notify();
+		}
+	}
+}
